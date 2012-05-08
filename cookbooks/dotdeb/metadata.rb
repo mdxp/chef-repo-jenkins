76a233e70beeb744daa6e9697c4453bcde1aee45
git-jenkins <git+jenkins@promethost.com>
2012-05-08 18:47:54 -0500

    2 cookbooks patch levels updated by Chef Jenkins
    php-fpm
    dotdeb

--- cookbooks/dotdeb/metadata.rb
+++ cookbooks/dotdeb/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Yevgeniy Viktorov"
 maintainer_email "wik@rentasite.com.ua"
 license          "Apache 2.0"
 description      "Dotdeb repositories"
-version          "0.1.1"
+version '0.1.2'
 recipe           "dotdeb", "main Dotdeb repository"
 recipe           "dotdeb::php53", "php53 Dotdeb repository"
 
